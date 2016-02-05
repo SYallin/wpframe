d8f77c665c50083c964f438b5b873e2769e3bda9
Alexander <bulgakov.developer@gmail.com>
2016-02-05 18:38:58 +0200

    removed the binding to the name of the folder

--- autoload.php
+++ autoload.php
@@ -1,24 +1,23 @@
 <?php
 
-define( 'WPFRAME_DIR', 'wpframe' );
+define( 'WPFRAME_DIR', dirname( __FILE__ ) );
 
 spl_autoload_register( function($class_name){
-	
-		if (file_exists( get_template_directory() . '/' . WPFRAME_DIR . '/' . $class_name .'/' . $class_name . '.php' )) {
-			require_once( get_template_directory() . '/' . WPFRAME_DIR . '/' . $class_name .'/' . $class_name . '.php' );
-		}elseif( file_exists( get_template_directory() . '/' . WPFRAME_DIR . '/Lib/' . $class_name . '.php' ) ){
-			require_once( get_template_directory() . '/' . WPFRAME_DIR . '/Lib/' . $class_name . '.php' );
-		}
-	
-		$class_arr = explode("_", $class_name);
-		$class_path = get_template_directory() . '/' . WPFRAME_DIR;
-		foreach($class_arr as $class ){
-			$class_path.= '/' . $class;
-			
-		}
-		$class_path.= '.php';
+	if (file_exists( WPFRAME_DIR . '/' . $class_name .'/' . $class_name . '.php' )) {
+		require_once( WPFRAME_DIR . '/' . $class_name .'/' . $class_name . '.php' );
+	}elseif( file_exists( WPFRAME_DIR . '/Lib/' . $class_name . '.php' ) ){
+		require_once( WPFRAME_DIR . '/Lib/' . $class_name . '.php' );
+	}
 
-		if (file_exists( $class_path )) {
-			require_once( $class_path );
-		}
+	$class_arr = explode("_", $class_name);
+	$class_path = WPFRAME_DIR;
+	foreach($class_arr as $class ){
+		$class_path.= '/' . $class;
+		
+	}
+	$class_path.= '.php';
+
+	if (file_exists( $class_path )) {
+		require_once( $class_path );
+	}
 } );
